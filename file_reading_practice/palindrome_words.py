2dd6b2f236dd9b8bd11010b6eb4f880684e21fb0
imca25mime18-crypto <imca.25mime18@silicon.ac.in>
2026-06-10 14:51:56 +0530

    Update palindrome_words.py

--- file_reading_practice/palindrome_words.py
+++ file_reading_practice/palindrome_words.py
@@ -51,3 +51,19 @@ Explanation:
 =================================================
 
 """
+def is_palindrome(word):
+    """Return True if word is a palindrome, otherwise False."""
+    return word == word[::-1]
+
+
+count = 0
+
+with open("sowpods.txt", "r") as file:
+    for line in file:
+        word = line.strip().lower()
+
+        if is_palindrome(word):
+            print(word)
+            count += 1
+
+print(f"Total palindromes: {count}")
